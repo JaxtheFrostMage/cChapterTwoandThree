f4e6b332a72f687c431aef12c0f41310466c609c
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-08-31 11:18:11 -0500

    gooder

--- Temp. Conversion/Temp. Conversion/Source.cpp
+++ Temp. Conversion/Temp. Conversion/Source.cpp
@@ -5,11 +5,18 @@
 #include <string>
 using namespace std;
 
+double convert(double fahren)
+{
+	double r;
+	r = (fahren - 32) * 5 / 9;
+	return r;
+}
 int main()
 {
-	int temp1 =0;
-	int temp2 =0;
-	int temp3 =0;
+	double temp1 =0;
+	double temp2 =0;
+	double temp3 =0;
+	double z;
 
 	cout << "What was the tempature at 8:00 am? ";
 	cin >> temp1;
@@ -20,5 +27,13 @@ int main()
 	cout << "What was the tempature at 5:00 pm? ";
 	cin >> temp3; 
 
+	z = convert(temp1);
+	cout << "your tempature at 8:00 am is  " << temp1 << " in Fahrenheit and " << z << " Celcius." << endl;
+	z = convert(temp1);
+	cout << "your tempature at 8:00 am is  " << temp1 << " in Fahrenheit and " << z << " Celcius." << endl;
+	z = convert(temp1);
+	cout << "your tempature at 8:00 am is  " << temp1 << " in Fahrenheit and " << z << " Celcius." << endl;
 
+	system("pause");
+	return 0;
 }
