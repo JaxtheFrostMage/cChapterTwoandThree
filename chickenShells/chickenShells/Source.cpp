c0be22699ebee25f56e4ab8f8728dc8df60956db
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-09-05 10:14:26 -0500

    Eggs are good

--- chickenShells/chickenShells/Source.cpp
+++ chickenShells/chickenShells/Source.cpp
@@ -6,26 +6,40 @@
 using namespace std;
 
 
-	int dozens(int eggs)
+	int convertToDozens(int eggs)
 {
-	int dozenEggs = 12 eggs;
+	int dozenEggs = eggs / 12;
+	return dozenEggs;
 }
 
-	int remainder(int eggs)
+	int findRemainder(int eggs)
 	{
-
+		int remainder = eggs % 12;
+		return remainder;
 	}
 
 	double totalCost(int eggs)
 	{
-
+		int dozenEggs = convertToDozens(eggs);
+		int remainder = findRemainder(eggs);
+		if (eggs > 120) {
+			double totalCost = dozenEggs * 1.50 + remainder * .25;
+			return totalCost;
+		}
+		else {
+			double totalCost = dozenEggs * 2.00 + remainder * .25;
+			return totalCost;
+		}
 	}
-
-
+		
 int main()
 {
 	int eggs;
 
 	cout << "how many eggs will you be buying? ";
 	cin >> eggs;
+	cout << "The cost of " << eggs << " eggs equals $" << totalCost(eggs) << "." << endl;
+
+system("pause");
+return 0;
 }
