1fbf1a57748eaba9749e755674d40f96e81f381a
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-09-06 10:33:29 -0500

    long and functional

--- Crispies Bagels and Bites/Crispies Bagels and Bites/Source.cpp
+++ Crispies Bagels and Bites/Crispies Bagels and Bites/Source.cpp
@@ -5,29 +5,29 @@
 #include <string>
 using namespace std;
 
-double findCost(double bagels, double donuts, double coffees)
+double findCost(double bagels, double donuts, double coffees, double bagelsCost, double donutsCost, double coffeesCost)
 {
-	double bagles = .99;
-	double donuts = .75;
-	double coffees = 1.20;
-	double totalCost = bagles + donuts + coffees;
+	double totalCost = bagels * bagelsCost + donuts * donutsCost + coffees * coffeesCost;
 	return totalCost;
 }
 int main()
 {
 
-	double bagels = 0;
-	double donuts = 0;
-	double coffees = 0;
+	double bagels;
+	double donuts;
+	double coffees;
+	double bagelsCost = .99;
+	double donutsCost = .75;
+	double coffeesCost = 1.20;
 
-	cout << "How many bagels have you purchased";
+	cout << "How many bagels have you purchased? " ;
 	cin >> bagels;
-	cout << "How many donuts have you purchased";
+	cout << "How many donuts have you purchased? " ;
 	cin >> donuts;
-	cout << "How many cups of coffee have you purchased";
+	cout << "How many cups of coffee have you purchased? " ;
 	cin >> coffees;
 
-	cout << "your total cost is $" << totalCost(bagels, donuts, coffees) << endl;
+	cout << "your total cost is $" << findCost(bagels, donuts, coffees, bagelsCost, donutsCost, coffeesCost) << endl;
 
 	system("pause");
 	return 0;
