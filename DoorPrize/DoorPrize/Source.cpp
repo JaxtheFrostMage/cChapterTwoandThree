5595d540ef002b851ac9e77f1e84bca70fd0f2eb
Evan Raker <er306632@students.davenport.k12.ia.us>
2018-09-11 11:17:57 -0500

    Make your choice

---
+++ DoorPrize/DoorPrize/Source.cpp
@@ -0,0 +1,112 @@
+//Random number generator
+//Author: Evan J. Raker
+//Date: 9/11/2018
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int doorGame()
+{
+	int userDoorChoice;
+	do {
+	cout << "Choose one of the three doors, one will save you, one will hurt you, the other will end you. \ndoor1/door2/door3 ";
+	cin >> userDoorChoice;
+
+	if (userDoorChoice == 1)
+		
+			{
+				cout << " (You reach your hand to the nob and as you do a spring loaded bear trap bites into your hand) \n The wrong door " << endl;
+			}
+		
+	else if (userDoorChoice == 2)
+	{
+		cout << "The door opens and you see a blinding light in the distance, it is the sun. \n Always take the right doors in life. " << endl;
+		}
+	else if (userDoorChoice == 3)
+	{
+		cout << "the door opens and you see a blinding light in the distance, it is the sun. as you step towards it you feel the ground below you crumble and you fall to your death. \n the wrong door and your last. " << endl;
+		}
+
+	else 
+	{
+		cout << "then you choose death. " << endl;
+		}
+	} while (userDoorChoice != 4);
+	return 0;
+}
+
+int randomGame()
+{
+	int numChoice;
+	int secret;
+	srand(time(NULL));
+	secret = rand() % 10 + 1;
+
+	cout << "A knack for gambling, are you willing to take the chance with lady luck? ";
+	cin >> numChoice; 
+
+	if (numChoice == secret)
+	{
+		cout << "The odds are in your favor..." << endl;
+	}
+	else
+	{
+		cout << "It seems you've run out of luck... " << secret << endl;
+	}
+	return 0;
+}
+
+int randomGame2()
+{
+	int numChoice;
+	int secret;
+	srand(time(NULL));
+	secret = rand() % 100 + 1;
+
+	cout << "A knack for gambling, are you willing to take the chance with lady luck? ";
+	cin >> numChoice;
+
+	if (numChoice == secret)
+	{
+		cout << "The odds are in your favor..." << endl;
+	}
+	else
+	{
+		cout << "It seems you've run out of luck... " << secret << endl;
+	}
+	return 0;
+}
+
+
+int main()
+{
+	int pickGame = 4;
+	int choice;
+	do {
+		cout << "Lets play a game, Pick your poison... \n doorGame = 1 RandomGame = 2 randomGame2= 3 Leave = 4" << endl;
+		cin >> pickGame;
+
+		if (pickGame == 1)
+		{
+			doorGame();
+		}
+		else if (pickGame == 2)
+		{
+			randomGame();
+		}
+		else if (pickGame == 3)
+		{
+			randomGame2();
+		}
+		else
+		{
+			cout << "you die" << endl;
+		}
+
+	} while (pickGame != 4);
+	system("pause");
+	return 0;
+}
